455f7301d953f521a7f0712d0fd8245266bfcf20
Vitor Bahir <vitorb82014@gmail.com>
2023-03-25 17:21:32 -0300

    Initial implementation

---
+++ interfaces2/Entities/Installment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace interfaces2.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueTime, double amount)
+        {
+            DueDate = dueTime;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ interfaces2/Services/ContractService.cs
@@ -0,0 +1,28 @@
+﻿using interfaces2.Entities;
+using System;
+
+namespace interfaces2.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime dueTime = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(basicQuota);
+                contract.AddInstallment(new Installment(dueTime, fullQuota));
+            }
+        }
+    }
+}
